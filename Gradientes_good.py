7f6b32d24a6da4085ed3cf4cc2eaa268b140fbe1
Esteban <esteban@oldandreliable.(none)>
2013-06-27 11:57:30 -0500

    final

--- Gradientes_good.py
+++ Gradientes_good.py
@@ -1,63 +1,80 @@
 import math
 import Image
+from time import time
+
+Sobelx = ([-1,0,1],[-2,0,2],[-1,0,1])
+Sobely = ([1,2,1],[0,0,0],[-1,-2,-1])
 
 
-def grises(ancho,altura,pixels,im):
-    for j in range(altura):
-        for i in range(ancho):
-            prom = sum(pixels[i,j])/3
-            pixels[i,j] = (prom,prom,prom)
+#Parametros: 
+#dimensiones de la imagen (w,h)
+#Arreglo de pixeles (pix)
+#La imagen misma (im)
+
+def grises(w,h,pix,im):
+    t_inicial = time()
+    #Recorremos el arreglo de pixeles
+    for j in range(h):
+        for i in range(w):
+            #se suman los valores R,G,B
+            #y se promedian al dividirlos entre 3
+            prom = sum(pix[i,j])/3
+            #el promedio es el nuevo valor de cada canal
+            pix[i,j] = (prom,prom,prom)
     im.save('gris.png')
     #im.show()
+    t_total = t_final - t_inicial
+    print "Tiempo en escala de grises: ",t_total
     return im 
 
 def convolucion(im):
-    ancho,altura = im.size
-    pixels = im.load()
-    prom = 0                 ##declaraicon de variable para promedio
+    t_inicial = time()
+    w,h = im.size
+    pix = im.load()
+    prom = 0  
     suma = 0
-    matrix = ([-1,0,1],[-2,0,2],[-1,0,1])
-    matriy = ([1,2,1],[0,0,0],[-1,-2,-1])
-    sumax = 0
-    sumay = 0
-    for j in range(altura):
-        for i in range(ancho):
+    
+    #Recorremos la imagen
+    for j in range(h):
+        for i in range(w):
             sumax = 0
             sumay = 0
-            for x in range(len(matrix[0])):
-                for y in range(len(matrix[0])):
+	    #Recorremos la mascara
+            for x in range(len(Sobelx[0])):
+                for y in range(len(Sobely[0])):
                     try:
-                    #multix = matrix[x][y]*pixels[i+y -1,j+x -1][1]
-                    #multiy = matriy[x][y]*pixels[i+y -1,j+x -1][1]
-                        multix = matrix[x][y]*pixels[i+y,j+x][1]
-                        multiy = matriy[x][y]*pixels[i+y,j+x][1]
+			#Multiplicacion de valores
+                        valorx = Sobelx[x][y]*pix[i+y,j+x][1]
+                        valory = Sobely[x][y]*pix[i+y,j+x][1]
 
                     except:
-                        multix = 0
-                        multiy = 0
-                    sumax = multix + sumax
-                    sumay = multiy + sumay
+                        valorx = 0
+                        valory = 0
+		    #Suma de multiplicaciones
+                    sumax = valorx + sumax
+                    sumay = valory + sumay
+	    #Distancia euclidiana
             xm = pow(sumax,2)
             ym = pow(sumay,2)
             g = int(math.sqrt(xm+ym))
-            suma = g
-        #suma = sumax + sumay
-        #print suma
-            if suma > 255:
-                suma = 255
-            if suma < 0:
-                suma = 0
-            pixels[i,j] = (suma,suma,suma)
+            if g > 255:
+                g = 255
+            if g < 0:
+                g = 0
+            pix[i,j] = (g,g,g)
     im.save('prueba.png')
     im.show()
+    t_final = time()
+    t_total = t_final - t_inicial
+    print "Tiempo de convolucion: ",t_total
 
 def main():
-    im = Image.open('mugrero.jpg')
+    im = Image.open('jenni.png')
     ancho,altura = im.size
     pixels = im.load()
 
-    #gris = grises(ancho,altura,pixels,im)
-    convolucion(im)
+    gris = grises(ancho,altura,pixels,im)
+    convolucion(gris)
 
 if __name__ == '__main__':
     main()
